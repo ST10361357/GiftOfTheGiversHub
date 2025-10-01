71dac7bf3018dcf7d67eb418bb422f145e70960d
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 23:31:48 +0000

    added Supply class

---
+++ Models/Supply.cs
@@ -0,0 +1,13 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    public class Supply
+    {
+        public int SupplyId { get; set; }  //primary key
+        public int ProjectID { get; set; }
+        public int DonationID {  get; set; } 
+        public string ItemName { get; set; }
+        public int Quantity { get; set; }
+        public string SupplyStatus { get; set; }
+        public DateTime DeliveryDate { get; set; }
+    }
+}
