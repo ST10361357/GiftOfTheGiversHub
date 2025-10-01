49a64197b1bdf87c7c876ebf56e0aa26923701fc
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 23:10:49 +0000

    add projecrs class

---
+++ Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; } 
+        
+        public string ProjectName { get; set; }
+
+        public string Region { get; set; }
+
+        public string Tpye { get; set; }
+
+        public string Status {  get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
