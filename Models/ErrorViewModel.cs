85edc717d865fdcec5797d0de5adeb775437a09a
RC_Student_lab <RC_Student_lab@labS5HDZL>
2025-10-01 22:28:55 +0000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GiftOfTheGiversHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
