5db2e6034e9e8d87eec70a1a2a14d6ecaf9db72b
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 23:43:49 +0000

    added User class

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }// primary key
+        public string UserName { get; set; }
+        public string UserEmail { get; set; }
+        public string Role { get; set; }// Admin, Donor, Volunteer
+        public string PasswordHash { get; set; }
+    }
+}
