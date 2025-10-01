aa073261e77e9a8a4c9c769ba259341f3ab8bcb1
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 23:06:27 +0000

    added Volunteer class with fields

---
+++ Models/Volunteer.cs
@@ -0,0 +1,12 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    public class Volunteer
+    {
+        public int VolunteerId { get; set; }// primary key
+        public string VolunteerName { get; set; }
+        public string VolunteerEmail {  get; set; } 
+        public string VolunteerPhone {  get; set; }
+        public string Skill {  get; set; }
+        public string Availability { get; set; }
+    }
+}
