b85801d631026e93d7722be4c0bc546328ebb5a0
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 23:19:55 +0000

    added Assignment class

---
+++ Models/Assignment.cs
@@ -0,0 +1,13 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    public class Assignment
+    {
+        public int AssignmentId { get; set; }//primary key
+        public int DonorID { get; set; }//FK from Donor class
+        public int VolunteerID {  get; set; }// FK from Volunteer class
+        public int ProjectID {  get; set; }//Fk from Project class
+        public string AssignRole { get; set; }//role in project
+        public DateTime AssignedDate { get; set; }
+
+    }
+}
