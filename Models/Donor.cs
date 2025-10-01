4ffa98ea39a68f1a694c086bc39d0e3ab1193e81
ST10361357 <ST10361357@rcconnect.edu.za>
2025-10-01 22:54:10 +0000

    added Donor class with fields

---
+++ Models/Donor.cs
@@ -0,0 +1,14 @@
+﻿namespace GiftOfTheGiversHub.Models
+{
+    //donor details
+    public class Donor
+    {
+        public int DonorID {get; set;}//primary key
+        public string DonorName {get; set;}
+        public string DonorEmail {get; set;}
+        public string DonorPhone {get; set;}
+        public string Organisation {get; set;}
+
+
+    }
+}
